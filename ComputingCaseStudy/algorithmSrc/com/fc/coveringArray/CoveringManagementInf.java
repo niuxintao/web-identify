3dc76f7ed5a77baff644ac80440fc214ec73d02d
niuxintao <niuxintao@gmail.com>
2014-05-27 23:06:01 +0800

    delete

--- ComputingCaseStudy/algorithmSrc/com/fc/coveringArray/CoveringManagementInf.java
+++
@@ -1,6 +0,0 @@
-package com.fc.coveringArray;
-
-public interface CoveringManagementInf {
-	public int setCover(Integer unCovered,int[] coveringArray,int[] row); //������һ��
-	public int rmCover(Integer unCovered,int[] coveringArray,int[] row);//������һ��
-}
