e5cb6563cc49cccb5ebf3147d3d16a6d316b8974
xintao <xintao@xintao-PC>
2015-01-06 00:16:33 +0800

    experiment is over

--- Error locating driven array/experiment/experimentData/TomcatData.java
+++ Error locating driven array/experiment/experimentData/TomcatData.java
@@ -25,35 +25,57 @@ public class TomcatData implements ExperimentData {
 
 	public void init() {
 		// TODO Auto-generated method stub
-		this.param = new int[] { 2, 2, 4, 2, 2, 2, 2, 2, 2, 3 };// parameters
+//	0	// 0 0
+		// 0 1
+//	1	// 1 0
+		// 1 1
+		
+		
+		
+		this.param = new int[] {2,  2, 2, 4, 2, 2, 2, 2, 2, 3 };// parameters
 
 		realMFS = new ArrayList<Tuple>();
 
-		int[] wrong = new int[] { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };// MFS
+		int[] wrong = new int[] {1, 0, 0, 3, 0, 0, 0, 0, 0, 0 };// MFS
 		TestCase wrongCase = new TestCaseImplement();
 		((TestCaseImplement) wrongCase).setTestCase(wrong);
 
 		Tuple bugModel1 = new Tuple(2, wrongCase);
-		bugModel1.set(0, 2);
+		bugModel1.set(0, 0);
 		bugModel1.set(1, 3);
 //		bugModel1.set(2, 4);
 		// bugModel1.set(1, 2);
 
-		int[] wrong2 = new int[] { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0};// MFS
+		int[] wrong2 = new int[] {1, 0, 0, 1, 0, 0, 0, 0, 0, 0};// MFS
 		TestCase wrongCase2 = new TestCaseImplement();
 		((TestCaseImplement) wrongCase2).setTestCase(wrong2);
 
 		Tuple bugModel2 = new Tuple(2, wrongCase2);
-		bugModel2.set(0, 2);
+		bugModel2.set(0, 0);
 		bugModel2.set(1, 3);
 //		bugModel2.set(2, 4);
 
+		
+		
+		int[] wrong3 = new int[] {1, 0, 0, 1, 0, 0, 0, 0, 0, 0};// MFS
+		TestCase wrongCase3 = new TestCaseImplement();
+		((TestCaseImplement) wrongCase3).setTestCase(wrong3);
+
+		Tuple bugModel3 = new Tuple(1, wrongCase3);
+//		bugModel3.set(0, 0);
+//		bugModel3.set(1, 3);
+		bugModel3.set(0, 8);
+		
+		
+		
 		realMFS.add(bugModel1);
 		realMFS.add(bugModel2);
+		realMFS.add(bugModel3);
 
 		this.caseRunner = new CaseRunnerWithBugInject();
 		((CaseRunnerWithBugInject) caseRunner).inject(bugModel1);
 		((CaseRunnerWithBugInject) caseRunner).inject(bugModel2);
+		((CaseRunnerWithBugInject) caseRunner).inject(bugModel3);
 
 	}
 
