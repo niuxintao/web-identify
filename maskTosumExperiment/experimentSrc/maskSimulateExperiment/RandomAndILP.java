9a57a6c17cc72962e8d6b236d24fd94ffaafa67c
xintao <xintao@114.212.86.184>
2015-03-28 16:04:46 +0800

    aaaaa

--- maskTosumExperiment/experimentSrc/maskSimulateExperiment/RandomAndILP.java
+++ maskTosumExperiment/experimentSrc/maskSimulateExperiment/RandomAndILP.java
@@ -3,7 +3,6 @@ package maskSimulateExperiment;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import com.fc.testObject.TestCase;
 import com.fc.tuple.Tuple;
 
@@ -14,11 +13,11 @@ public class RandomAndILP {
 	public static final int replaceTime = 1;
 	public static final int replace = 2;
 
-	private double[] data;
+	private double[][] data;
 
 	public RandomAndILP() {
 		setup = new ExpriSetUp();
-		data = new double[3];
+		data = new double[2][3];
 	}
 
 	public void test(int index) {
@@ -42,7 +41,7 @@ public class RandomAndILP {
 			bench.addAll(setup.getBugsList().get(key));
 		}
 
-//		unit.setBugs(bench);
+		// unit.setBugs(bench);
 
 		int allNum = 0;
 		for (Integer code : exData.getWrongCases().keySet()) {
@@ -55,29 +54,29 @@ public class RandomAndILP {
 
 		}
 
-	
-		for (int i : new int[] { 9 }) {
+		for (int i : new int[] { UnitSimulate.MASK_FIC,
+				UnitSimulate.MASK_FIC_OLD }) {
 			// System.out.println(i);
+			int j = (i == UnitSimulate.MASK_FIC ? 0 : 1);
 			// for (TestCase testCase : this.additionalTestCases.get(i))
-			if (allNum > 0){
-				data[replace] = unit.getReplacingTimes().get(i).size()
+			if (allNum > 0) {
+				data[j][replace] = unit.getReplacingTimes().get(i).size()
 						/ (double) allNum;
-				
-				
+
 				double avRpel = 0;
-				for(Integer it : unit.getReplacingTimes().get(i)){
+				for (Integer it : unit.getReplacingTimes().get(i)) {
 					avRpel += it.intValue();
 				}
 				avRpel /= (double) (unit.getReplacingTimes().get(i).size());
-				data[replaceTime] = avRpel;
-				
+				data[j][replaceTime] = avRpel;
+
 				double avRpel2 = 0;
-				for(Long it : unit.getTimeMillions().get(i)){
+				for (Long it : unit.getTimeMillions().get(i)) {
 					avRpel2 += it.longValue();
 				}
 				avRpel2 /= (double) (unit.getTimeMillions().get(i).size());
-				data[millions] = avRpel2; 
-				
+				data[j][millions] = avRpel2;
+
 			}
 
 		}
@@ -85,48 +84,49 @@ public class RandomAndILP {
 	}
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 1; i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println();
 			System.out.println("the " + i + " th");
-			double[][] datas = new double[30][];
+			double[][][] datas = new double[30][2][];
 			for (int j = 0; j < 30; j++) {
 				RandomAndILP ex = new RandomAndILP();
 				ex.test(i);
 				datas[j] = ex.data;
 			}
-			
-			double avg = 0;
-			System.out.print("replace :");
-			for (int j = 0; j < 30; j++) {
-				System.out.print(datas[j][replace] + " ");
-				avg += datas[j][replace];
-			}
-			avg /= 30;
-			System.out.println();
-			System.out.println("avg : " + avg);
-			
-			avg = 0;	
-			System.out.print("replaceTime :");
-			for (int j = 0; j < 30; j++) {
-				System.out.print(datas[j][replaceTime] + " ");
-				avg += datas[j][replaceTime];
-			}
-			avg /= 30;
-			System.out.println();
-			System.out.println("avg : " + avg);
-			
-			avg = 0;
-			System.out.print("replaceTime :");
-			for (int j = 0; j < 30; j++) {
-				System.out.print(datas[j][millions] + " ");
-				avg += datas[j][millions];
-			}
-			avg /= 30;
-			System.out.println();
-			System.out.println("avg : " + avg);
-			avg = 0;
 
+			for (int k = 0; k < 2; k++) {
+				System.out.println("the " + k + " th");
+				double avg = 0;
+				System.out.print("replace :");
+				for (int j = 0; j < 30; j++) {
+					System.out.print(datas[j][replace] + " ");
+					avg += datas[j][k][replace];
+				}
+				avg /= 30;
+				System.out.println();
+				System.out.println("avg : " + avg);
 
+				avg = 0;
+				System.out.print("replaceTime :");
+				for (int j = 0; j < 30; j++) {
+					System.out.print(datas[j][replaceTime] + " ");
+					avg += datas[j][k][replaceTime];
+				}
+				avg /= 30;
+				System.out.println();
+				System.out.println("avg : " + avg);
+
+				avg = 0;
+				System.out.print("replaceTime :");
+				for (int j = 0; j < 30; j++) {
+					System.out.print(datas[j][millions] + " ");
+					avg += datas[j][k][millions];
+				}
+				avg /= 30;
+				System.out.println();
+				System.out.println("avg : " + avg);
+				avg = 0;
+			}
 		}
 		// ex.test(0);
 		// ex.test(2);
