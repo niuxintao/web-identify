01bc6607f50e3c43d040c75a37eb579f72e6b250
xintao <xintao@114.212.82.248>
2016-05-31 11:35:35 +0800

    aaa

--- jlan-3.4-test/src/lantest/LanTest.java
+++ jlan-3.4-test/src/lantest/LanTest.java
@@ -1,5 +1,62 @@
 package lantest;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class LanTest {
 
+//	public void testIsNumber(boolean isDig) {
+//		String a = "";
+//		if (isDig)
+//			a = "81.5514";
+//		else
+//			a = "81.5514";
+//		System.out.println(NumberUtils.isNumber(a));
+//	}
+
+	public void testTrail(int isMutiple) {
+
+		String number = "";
+		if (isMutiple == 0)
+			number = "81.5514DD";
+		else if (isMutiple == 1)
+			number = "81.5514D";
+		else
+			number = "81.5514";
+
+		boolean isNumber = NumberUtils.isNumber(number); // returns false
+		boolean result = false;
+
+		try {
+			NumberUtils.createNumber(number); // returns
+			if (isNumber == true)
+				result = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (isNumber == false)
+				result = true;
+			else
+				result = false;
+		}
+
+		System.out.println(result);
+	}
+
+	public void testAccurancyLoss(boolean isGreaterThanSeven) {
+		String number = "";
+		if (isGreaterThanSeven)
+			number = "193343.82";
+		else
+			number = "1.1";
+
+		System.out.println(number.equals(NumberUtils.createNumber(number)
+				.toString()));
+	}
+
+	public static void main(String[] args) {
+		LanTest lt = new LanTest();
+
+		lt.testAccurancyLoss(true);
+
+		lt.testTrail(2);
+	}
 }
